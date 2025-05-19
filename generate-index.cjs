a94013493fbc5abe838b77873c727646f546a889
massinissa djemouai <djemouaimassinissa13@gmail.com>
2025-05-20 00:29:17 +0200

    Fix generate-index.xjs

--- generate-index.cjs
+++ generate-index.cjs
@@ -2,8 +2,6 @@
 const fs = require("fs");
 const path = require("path");
 
-const __dirname = __dirname; // disponible en CJS
-
 const repo = process.env.GITHUB_REPOSITORY || "demo/demo";
 const [owner, name] = repo.split("/");
 const branch = "main";
