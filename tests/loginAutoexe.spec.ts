b88492c3065394c931fd70cc1cb3f6f52ec4f206
massinissa djemouai <djemouaimassinissa13@gmail.com>
2025-05-23 19:38:36 +0200

    Fix loginAutoexe

--- tests/loginAutoexe.spec.ts
+++ tests/loginAutoexe.spec.ts
@@ -1,54 +1,74 @@
-import { test } from '../fixtures/user-fixture';
-import { AllureUtils } from '../src/utils/allure.utils';
-      
+import { test } from "../fixtures/user-fixture";
+import { AllureUtils } from "../src/utils/allure.utils";
 
-test('🔐 Connexion d’un utilisateur existant ', () => {
-  test(' ✔ Connexion d’un utilisateur existant avec des identifiants valide',{tag: ['@login', '@sanity'], annotation: [
-    { type: 'Jira Story', 
-      description: 'https://jira.com/AutomationExe' },
-  ]}, async ({ page, user,loginAutoexePage }) => {
+test(
+  "✔ Connexion d’un utilisateur existant avec des identifiants valides",
+  {
+    tag: ["@login", "@sanity"],
+    annotation: [
+      { type: "Jira Story", description: "https://jira.com/AutomationExe" },
+    ],
+  },
+  async ({ page, user, loginAutoexePage }) => {
+    AllureUtils.initSuite(
+      "AutomationExe Project",
+      "AutomationExe",
+      "Authentification"
+    );
+    AllureUtils.setDescription(
+      "Ce test vérifie l'authentification avec des identifiants valides."
+    );
+    AllureUtils.addTags("login", "sanity");
+    AllureUtils.setSeverity("critical");
 
-    AllureUtils.initSuite('AutomationExe Project', 'AutomationExe', 'Authentification');
-    AllureUtils.setDescription('Ce test a pour but de vérifier l\'authentification avec des identifiants valides.');
-    AllureUtils.addTags('login', 'sanity');
-    AllureUtils.setSeverity('critical');
-   
     await loginAutoexePage.goto();
-    
-    await test.step("1. Saisir le nom ed'utilisateur et le mot de passe", async () => {
-    await loginAutoexePage.acceptCockies();
-    await loginAutoexePage.login(user.email, user.password);
-    AllureUtils.attachJson('Email utilisateur', user.email);
-    AllureUtils.attachJson('Password utilisateur', user.password);
+
+    await test.step("1. Saisir l'email et le mot de passe", async () => {
+      await loginAutoexePage.acceptCockies();
+      await loginAutoexePage.login(user.email, user.password);
+      AllureUtils.attachJson("Email utilisateur", user.email);
+      AllureUtils.attachJson("Password utilisateur", user.password);
     });
 
-    await test.step("2. Vérifier si l'utilisateur c'est bien authentifier", async () => {
-    await loginAutoexePage.expectLoginSuccess();
-    await AllureUtils.attachScreenshot('Dashboard', page);
+    await test.step("2. Vérifier la connexion réussie", async () => {
+      await loginAutoexePage.expectLoginSuccess();
+      await AllureUtils.attachScreenshot("Dashboard après connexion", page);
     });
-});
+  }
+);
 
-test(' ❌ Connexion d’un utilisateur existant mais avec un mot de passe erroné',{tag: ['@login', '@sanity'], annotation: [
-    { type: 'Jira Story', 
-      description: 'https://jira.com/AutomationExe' },
-  ]}, async ({ page, user,loginAutoexePage }) => {
+test(
+  "❌ Connexion échouée avec un mot de passe invalide",
+  {
+    tag: ["@login", "@sanity"],
+    annotation: [
+      { type: "Jira Story", description: "https://jira.com/AutomationExe" },
+    ],
+  },
+  async ({ page, user, loginAutoexePage }) => {
+    AllureUtils.initSuite(
+      "AutomationExe Project",
+      "AutomationExe",
+      "Authentification"
+    );
+    AllureUtils.setDescription(
+      "Ce test vérifie que la connexion échoue avec un mauvais mot de passe."
+    );
+    AllureUtils.addTags("login", "sanity");
+    AllureUtils.setSeverity("critical");
 
-    AllureUtils.initSuite('AutomationExe Project', 'AutomationExe', 'Authentification');
-    AllureUtils.setDescription('Ce test a pour but de vérifier l\'authentification avec des identifiants invalide.');
-    AllureUtils.addTags('login', 'sanity');
-    AllureUtils.setSeverity('critical');
-    
     await loginAutoexePage.goto();
-    await test.step("1. Saisir le nom ed'utilisateur et le mot de passe", async () => {
-    await loginAutoexePage.acceptCockies();
-    await loginAutoexePage.login(user.email, 'wrong-password');
-    AllureUtils.attachJson('Email utilisateur', user.email);
-    AllureUtils.attachJson('Password utilisateur', user.password);
+
+    await test.step("1. Saisir l'email et un mauvais mot de passe", async () => {
+      await loginAutoexePage.acceptCockies();
+      await loginAutoexePage.login(user.email, "wrong-password");
+      AllureUtils.attachJson("Email utilisateur", user.email);
+      AllureUtils.attachJson("Password utilisé", "wrong-password");
     });
 
-    await test.step("2. Vérifier si l'utilisateur a un message d'erreur", async () => {
-    await AllureUtils.attachScreenshot('Dashboard', page);
-    await loginAutoexePage.expectLoginFail();
+    await test.step("2. Vérifier le message d'erreur", async () => {
+      await loginAutoexePage.expectLoginFail();
+      await AllureUtils.attachScreenshot("Message d'erreur affiché", page);
     });
-  });
-});
+  }
+);
