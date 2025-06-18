4ee3897210e7d421b1c884651e773ee4cd5d2b1a
massinissa djemouai <djemouaimassinissa13@gmail.com>
2025-06-18 12:19:24 +0200

    Fix File conf

--- src/page-objects/automationExe/e2e/ProductsAutoexePage.ts
+++ src/page-objects/automationExe/e2e/ProductsAutoexePage.ts
@@ -1,6 +1,6 @@
 import { Page, Locator, expect } from "@playwright/test";
 import { getRandomElement } from "../../../utils/randomUtils";
-import { Categories } from "../../../test-data";
+import { Categories } from "../../../test-data/qa/Categories";
 
 export class ProductsAutoexePage {
   private searchProductField: Locator;
